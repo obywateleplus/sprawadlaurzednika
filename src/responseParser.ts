cc747774ef082718318c2bd61a7286ab14be689b
0x4352430A <59255191+0x4352430A@users.noreply.github.com>
2022-10-21 14:31:02 +0200

    Delete src directory

--- src/responseParser.ts
+++
@@ -1,9 +0,0 @@
-import {Response} from 'express';
-
-export function respondNotFound(res: Response, message: string) {
-    res.send({"code": 404, "message": message});
-};
-
-export function respondUnAuthorized(res: Response, message: string) {
-    res.send({"code": 401, "message": message});
-};
--- src/routers/ticketRouter.ts
+++
@@ -1,8 +0,0 @@
-import express, {Request, Response, Router} from 'express';
-import { database } from '../vars';
-import {respondNotFound} from '../responseParser';
-
-export const ticketRouter: Router = express.Router();
-
-ticketRouter.route('/ticket')
-.get(async(req: Request, res: Response) => {});
