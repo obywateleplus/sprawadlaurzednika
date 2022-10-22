21bf6c32d085d39de24d596285e3d4ca6efa2456
0x4352430A <59255191+0x4352430A@users.noreply.github.com>
2022-10-22 12:49:50 +0200

    Final release*

--- src/routers/chatRouter.ts
+++ src/routers/chatRouter.ts
@@ -8,13 +8,17 @@ chatRouter.route('/chat')
 .get(async(req: Request, res: Response) => {
     const data = new URLSearchParams(req.url.substring(req.url.indexOf('?'), req.url.length));
     const pool = database?.promise();
-    const chat: any = await pool?.execute("SELECT * FROM   WHERE `obywateleplus`.`chatsave`.`ticketid` = ?;", [data.get('ticket')]);
-    if (chat[0][0] === undefined) respondNotFound(res, `Cannot find chat save with ticetid ${data.get('ticket')}`);
-    res.send(chat[0][0]);
+    const chat: any = await pool?.execute("SELECT * FROM `obywateleplus`.`chatsave` WHERE `obywateleplus`.`chatsave`.`ticketid` = ?;", [data.get('ticket')]);
+    if (chat[0][0] === undefined) {
+        respondNotFound(res, `Cannot find chat save with ticetid ${data.get('ticket')}`);
+    } else {
+        res.send(chat[0]);
+    }
+    
 })
 .post(async(req: Request, res: Response) => {
     const data = req.body;
     const pool = database?.promise();
-    const result: any = await pool?.execute("INSERT INTO `obywateleplus`.`chatsave` VALUES (?, ?, ?);", [data.ticket, data.sent, data.ticket]);
+    const result: any = await pool?.execute("INSERT INTO `obywateleplus`.`chatsave` VALUES (?, ?, ?, ?);", [data.sender, data.content, data.sent, data.ticket]);
     res.send(result);
 });
